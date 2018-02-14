39acba2d36e46222d5b35e026074307c52bd7898
CaioRS <caiorss.rodrigues@gmail.com>
2018-02-14 15:29:21 -0300

    Add netmon.scala as package description file.

---
+++ netmon.scala
@@ -0,0 +1,8 @@
+/**   
+     Namespace containing network monitoring facilities.
+     @author       Caio Rodrigues <caiorss.rodrigues@gmail.com>
+     @description  Network Monitoring Application 
+
+*/
+package object netmon {
+}
