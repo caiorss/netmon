f3886c8a1be9aee4a07f14f4caac8ba96fa491bf
CaioRS <caiorss.rodrigues@gmail.com>
2018-02-14 10:31:50 -0300

    Improve file organization and modularity.

--- netmon.scala
+++ netmon.scala
@@ -5,124 +5,128 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 
+object Utils{
+  def runEvery(period: Int)(action: => Unit) =
+    while(true){
+      action
+      java.lang.Thread.sleep(period)
+    }
 
-/// Show computation execution time
-def executionTime[A](comp: => A){
-  val t1 = new java.util.Date()
-  comp
-  val t2 = new java.util.Date()
-  val dt = (t2.getTime - t1.getTime).toDouble / 1000.0 
-  println(s"This computation took ${dt} seconds.")
+  def getResourceImage(file: String, cls: Class[_]) = {  
+    val uri = cls.getResource(file)
+    assert(uri != null, s"Error resource file $file not found")
+    java.awt.Toolkit.getDefaultToolkit().getImage(uri)
+  }
+
+  /// Show computation execution time
+  def executionTime[A](comp: => A){
+    val t1 = new java.util.Date()
+    comp
+    val t2 = new java.util.Date()
+    val dt = (t2.getTime - t1.getTime).toDouble / 1000.0 
+    println(s"This computation took ${dt} seconds.")
+  }
+  
 }
 
-def getInterfaces(): Map[String,java.net.InetAddress] = {
-  import java.net.InetAddress
-  import java.net.NetworkInterface
-  import collection.JavaConverters._
+object NetInfo{
 
-  def getInterfaceAddress(net: NetworkInterface) = {
-    net.getInterfaceAddresses()
+  def checkHTTP(hostname: String)(handler: (Boolean, String) => Unit) = { 
+    val fut = Future{checkHTTPSync(hostname)}
+    try {
+      Await.result(fut, 2.second)
+      handler(true, "Connection OK")
+    } catch {
+      case ex: java.net.UnknownHostException
+          => try {
+            // address of www.google.com = 216.58.222.100
+            if(isPortOpen("216.58.222.100", 80))
+              handler(false, "Error: DNS Failure\nbut connection works (can connect to 216.58.222.100)\ntry changing DSN")
+            else 
+              handler(false, "Error: DNS Failure")
+          } catch {
+            case ex: java.net.SocketException
+                => handler(false, "Network is unreachable (connect failed) + DNS Failure")
+          }
+      case ex: java.net.SocketException
+          => handler(false, "Network is unreachable (connect failed)")
+      case ex: java.util.concurrent.TimeoutException
+          => handler(false, "Error: Connection very slow, timeout exceeded")
+    }
+  }  
+
+  def isPortOpen(address: String, port: Int, timeout: Int = 1000) = {
+    import java.net.Socket
+    import java.net.InetSocketAddress
+    try {
+      val sock = new Socket()
+      sock.connect(new InetSocketAddress(address, port), timeout)
+      sock.close()
+      true
+    } catch {
+      // Connection refused exception
+      case (ex: java.net.ConnectException)
+          => false
+      case (ex: java.net.SocketTimeoutException)
+          => false
+      case ex: Throwable => throw ex
+    }
+  }
+
+  def checkHTTPSync(hostname: String) =  {
+    var is: java.io.InputStream = null  
+    val c = new java.net.URL("http://" + hostname).openConnection()
+    try {
+      is = c.getInputStream()
+      val buffer = new Array[Char](50)
+      val br = new java.io.BufferedReader(
+        new java.io.InputStreamReader(is)
+      )   
+      br.read(buffer, 0, 50)
+      val line = buffer.mkString
+      //println("line = " + line)
+      line 
+    } finally{
+      if(is != null) is.close()
+    }
+  }
+
+  def getInterfaces(): Map[String,java.net.InetAddress] = {
+    import java.net.InetAddress
+    import java.net.NetworkInterface
+    import collection.JavaConverters._
+
+    def getInterfaceAddress(net: NetworkInterface) = {
+      net.getInterfaceAddresses()
+        .asScala
+        .toSeq
+        .find(inf => inf.getBroadcast() != null)
+        .map(_.getAddress())
+    }
+
+    val interfaces = NetworkInterface
+      .getNetworkInterfaces()
       .asScala
       .toSeq
-      .find(inf => inf.getBroadcast() != null)
-      .map(_.getAddress())
-  }
+    interfaces.filter(ni =>
+      !ni.isLoopback()          // Exclude loopback interfaces
+        && ni.isUp()            // Select active interface
+        && ni.getHardwareAddress() != null
+        && getInterfaceAddress(ni).isDefined
+    )
+      .map(ni => (ni.getName(), getInterfaceAddress(ni).get))
+      .toMap
+  } // ----- EOF function getInterfaces() -------- //
 
-  val interfaces = NetworkInterface
-    .getNetworkInterfaces()
-    .asScala
-    .toSeq
-  interfaces.filter(ni =>
-    !ni.isLoopback()          // Exclude loopback interfaces
-      && ni.isUp()            // Select active interface
-      && ni.getHardwareAddress() != null
-      && getInterfaceAddress(ni).isDefined
-  )
-    .map(ni => (ni.getName(), getInterfaceAddress(ni).get))
-    .toMap
-} // ----- EOF function getInterfaces() -------- //
+  def checkDNS() =
+    try {
+      java.net.InetAddress.getByName("www.google.com")
+      true
+    } catch {
+      case ex: java.net.UnknownHostException => false
+    }
 
-
-def checkDNS() =
-  try {
-    java.net.InetAddress.getByName("www.google.com")
-    true
-  } catch {
-    case ex: java.net.UnknownHostException => false
-  }
-
-// def checkWAN() =
-//   // Google's www.google.com 
-//   isPortOpen("216.58.222.100", 80)
-
-def isPortOpen(address: String, port: Int, timeout: Int = 1000) = {
-  import java.net.Socket
-  import java.net.InetSocketAddress
-  try {
-    val sock = new Socket()
-    sock.connect(new InetSocketAddress(address, port), timeout)
-    sock.close()
-    true
-  } catch {
-    // Connection refused exception
-    case (ex: java.net.ConnectException)
-        => false
-    case (ex: java.net.SocketTimeoutException)
-        => false
-    case ex: Throwable => throw ex
-  }
 }
-
-
-def checkHTTPSync(hostname: String) =  {
-  var is: java.io.InputStream = null  
-  val c = new java.net.URL("http://" + hostname).openConnection()
-  try {
-    is = c.getInputStream()
-    val buffer = new Array[Char](50)
-    val br = new java.io.BufferedReader(
-      new java.io.InputStreamReader(is)
-    )   
-    br.read(buffer, 0, 50)
-    val line = buffer.mkString
-    //println("line = " + line)
-    line 
-  } finally{
-    if(is != null) is.close()
-  }
-}
-
-// case class NetworkStatusHandler(
-//   NetworkOK:           String => Unit,
-//   NetwrokDNSFailLure:  String => Unit,
-//   NetworkUnreachable : String => Unit,
-// )
-
-
-def checkHTTP(hostname: String)(handler: (Boolean, String) => Unit) = { 
-  val fut = Future{checkHTTPSync(hostname)}
-  try {
-    Await.result(fut, 2.second)
-    handler(true, "Connection OK")
-  } catch {
-    case ex: java.net.UnknownHostException
-        => try {
-          // address of www.google.com = 216.58.222.100
-          if(isPortOpen("216.58.222.100", 80))
-            handler(false, "Error: DNS Failure\nbut connection works (can connect to 216.58.222.100)\ntry changing DSN")
-          else 
-            handler(false, "Error: DNS Failure")
-        } catch {
-          case ex: java.net.SocketException
-              => handler(false, "Network is unreachable (connect failed) + DNS Failure")
-        }
-    case ex: java.net.SocketException
-        => handler(false, "Network is unreachable (connect failed)")
-    case ex: java.util.concurrent.TimeoutException
-        => handler(false, "Error: Connection very slow, timeout exceeded")
-  }
-}
-
 
 class Display extends javax.swing.JFrame{
   private val out = new javax.swing.JTextArea()
@@ -132,7 +136,7 @@ class Display extends javax.swing.JFrame{
 
     val frame = this
     frame.setTitle("Internet Connection Status")
-    frame.setSize(300, 100)
+    frame.setSize(400, 120)
     // frame.setLayout(new java.awt.FlowLayout())
     frame.add(new javax.swing.JScrollPane(out))
     frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE)
@@ -144,39 +148,28 @@ class Display extends javax.swing.JFrame{
     out.setText(msg)
 }
 
+object Main{ 
+  val iconOnline  =
+    Utils.getResourceImage("/resources/network-online.jpg", getClass())
 
-def runEvery(period: Int)(action: => Unit) =
-  while(true){
-    action
-    java.lang.Thread.sleep(period)
+  val iconOffline =
+    Utils.getResourceImage("/resources/network-offline.png", getClass())
+
+  def main(args: Array[String]) = {
+    val disp = new Display()
+    disp.setIconImage(iconOnline)
+
+    // Run action every 2000 milliseconds or 
+    Utils.runEvery(2000){
+      val time = new java.util.Date()
+      NetInfo.checkHTTP("www.google.com"){ case (status, msg) =>
+        disp.display (msg + "\n Last Update : " + time.toString())
+        if(status)
+          disp.setIconImage(iconOnline)
+        else
+          disp.setIconImage(iconOffline)
+      }
+    }
   }
+} // --- End of object Main() ------ // 
 
-
-def getResourceImage(file: String, cls: Class[_]) = {  
-  val uri = cls.getResource(file)
-  assert(uri != null, s"Error resource file $file not found")
-  java.awt.Toolkit.getDefaultToolkit().getImage(uri)
-}
-
-// val iconOnline  = getResourceImage("/resources/network-online.jpg", getClass())
-// val iconOffline = getResourceImage("/resources/network-error.png", getClass())
-
-val iconOnline  = getResourceImage("/resources/network-online.jpg", getClass())
-val iconOffline = getResourceImage("/resources/network-offline.png", getClass())
-
-println("Icon = " + iconOnline)
-
-val disp = new Display()
-disp.setIconImage(iconOnline)
-
-// Run action every 2000 milliseconds or 
-runEvery(2000){
-  val time = new java.util.Date()
-  checkHTTP("www.google.com"){ case (status, msg) =>
-    disp.display (msg + "\n Last Update : " + time.toString())
-    if(status)
-      disp.setIconImage(iconOnline)
-    else
-      disp.setIconImage(iconOffline)
-  }
-}
