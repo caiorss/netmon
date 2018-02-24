43c3587eba280ea061e48880294e7820a2920251
CaioRS <caiorss.rodrigues@gmail.com>
2018-02-24 14:33:23 -0300

    Create installer to generate Linux's desktop icon.

---
+++ installer.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+SHORT_DESCRIPTION="Netmon - network monitor"
+APPNAME=netmon
+TEMPFILE=/tmp/installer-app.desktop
+BINARY=./bin/netmon-guard.jar
+DEST=~/opt/jarapps/$APPNAME
+DESTFILE=netmon.jar 
+ICON=./resources/network-online.jpg
+
+echo "PWD = "$(pwd)
+
+mkdir -p $DEST
+
+# Copy executable to installation directory 
+cp -v $BINARY $DEST/$DESTFILE
+
+# Copy Icon to installation directory
+cp -v $ICON $DEST 
+
+cat <<EOF >> $TEMPFILE
+[Desktop Entry]
+Name=$SHORT_DESCRIPTION
+Exec=java -jar $DEST/$DESTFILE
+Icon=$DEST/network-online.jpg
+Type=Application
+Terminal=false
+Categories=Tools;
+EOF
+
+# Display desktop file 
+cat $TEMPFILE
+
+# Create desktop shortcut 
+cp -v $TEMPFILE ~/Desktop/$APPNAME.desktop
+
+# Install into user Menu 
+cp -v $TEMPFILE ~/.local/share/applications/$APPNAME.desktop 
+
+rm -rf $TEMPFILE
+echo "Removed temporary file "$TEMPFILE
